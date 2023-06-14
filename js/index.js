0402124c89b49dd4daa6480374461d6ee1bac1fd
sebasmaturanac <sebasmaturanac@gmail.com>
2023-06-14 12:02:24 -0300

    Cambio Index_es, landing

--- js/index.js
+++ js/index.js
@@ -8,3 +8,24 @@ function redirigirAPaginaPrincipal() {
 
 // Esperar cierto tiempo y luego redirigir
 setTimeout(redirigirAPaginaPrincipal, tiempoEspera);
+
+
+
+
+// EFECTO INDEX_ES
+
+(function($){
+  // variables
+  elementWidth = $('ul').width(),
+  containerWidth = $('nav').width(),
+  difference = elementWidth-containerWidth,
+  finalWidth = difference * 1.5,
+  element = $('ul');
+  
+  // active on click
+  $('li').on('click', function(){
+    $('li').removeClass('active');
+    $(this).addClass('active');
+  });
+  
+})(jQuery);
