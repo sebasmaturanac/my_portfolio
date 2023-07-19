3e4d53e8469e494bdd28b06f1928ea762cdaa599
sebasmaturanac <sebasmaturanac@gmail.com>
2023-07-19 13:08:10 -0300

    fix bug

--- js/index.js
+++ js/index.js
@@ -12,20 +12,3 @@ setTimeout(redirigirAPaginaPrincipal, tiempoEspera);
 
 
 
-// EFECTO INDEX_ES
-
-(function($){
-  // variables
-  elementWidth = $('ul').width(),
-  containerWidth = $('nav').width(),
-  difference = elementWidth-containerWidth,
-  finalWidth = difference * 1.5,
-  element = $('ul');
-  
-  // active on click
-  $('li').on('click', function(){
-    $('li').removeClass('active');
-    $(this).addClass('active');
-  });
-  
-})(jQuery);
